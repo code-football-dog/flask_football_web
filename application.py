9d1a8f65f7ee2cc4e39a2cb069b595c1e8da132e
code_wht_123 <2472645764@qq.com>
2018-05-08 18:06:45 +0800

    no message

--- application.py
+++ application.py
@@ -3,10 +3,11 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SERVER_NAME'] = 'lehss.top'
 
 @app.route("/")
 def index():
     return "hello boy welcome to lehss.top"
 
 if __name__ == '__main__':
-    app.run()
+    app.run("0.0.0.0")
